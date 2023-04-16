b461c556d5b59e242017632f8223ef280042831c
Vincius Vieira <Viniividrade@gmail.com>
2023-04-16 12:55:48 -0300

    realizado todos os exercicios dos assuntos gerais abordados em aula do dia 14.04.23 exercicios sobre vetor, condicional e repeticao

--- lista-exercicios-conhecimento-geral/exercicio7/Program.cs
+++ lista-exercicios-conhecimento-geral/exercicio7/Program.cs
@@ -2,3 +2,22 @@
 // 15 números, o algoritmo deve escrever esses 15 números lidos na ordem inversa da qual foi
 // declarado.
 
+// 1. ler 15 numeros e guarda-los num vetor(array)
+// 2. escrever os 15 numeros na ordem inversa da qual foi escrita/declarada
+
+int[] numeros = new int[15];
+for (int i = 0; i < 15; i++)
+{
+    Console.WriteLine($"Insira o {i + 1}º");
+    numeros[i] = int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine($"Os numeros digitados na ordem inversa ficarão da seguinte maneira:");
+
+foreach (int item in numeros.Reverse())
+{
+    Console.WriteLine($"{item}");
+}
+
+
+
